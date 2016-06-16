a37debecde2fdb4e8211f175557d95701860e4bb
Eric Buss <ebuss@ualberta.ca>
2016-06-16 11:47:26 -0700

    Started interface for accounts

---
+++ public/js/main.js
@@ -0,0 +1,106 @@
+//==========================================================================================================================//
+//Global Variables                                                                                                    
+//==========================================================================================================================//
+var vc = new ViewController('.content');
+//==========================================================================================================================//
+// Login screen functions                                                                                                        
+//==========================================================================================================================//
+function newAccount(username, password, message) {
+    
+    console.log('newAccount called with username: ' + username + ' password: ' + password);
+    
+    // Check new account with server
+    
+    var canCreateAccount = true; 
+    
+    // Handle error messages (account already in use, blank username/password, etc.)
+    
+    if (!canCreateAccount) {
+        vc.banner('This user account is already taken');
+        return;
+    }
+    
+    // Get new account info 
+    
+    player = null;
+    
+    playerManager = new PlayerManager(player);
+    
+    vc.menu(playerManager);
+}
+
+function login(username, password, message) {
+    
+    console.log('login called with username: ' + username + ' password: ' + password);
+    
+    // Check if valid login
+    
+    var canLogin = false;
+    
+    // Handle error messages (wrong password, blank username/password, etc.)
+    
+    if (!canLogin)  {
+        vc.banner('Wrong password')
+        return;
+    }
+        
+    // Get account info
+        
+    player = null;
+    
+    playerManager = new PlayerManager(player);
+    
+    vc.menu(playerManager);
+}
+//==========================================================================================================================//
+// Story Manager                                                                                                        
+//==========================================================================================================================//
+function StoryManager(level) {
+    
+}      
+//==========================================================================================================================//
+// Game Manager                                                                                                        
+//==========================================================================================================================//
+function GameManager(ai, stage, size, gvc) {
+    
+}
+//==========================================================================================================================//
+// ReplayManager                                                                                                        
+//==========================================================================================================================//
+function ReplayManager(game, gvc) {
+    
+    // Hook GameViewController replay settings
+    
+    gvc.playPause = function() {
+        
+    }
+    
+    gvc.fastForward = function() {
+        
+    }
+    
+    gvc.rewind = function() {
+        
+    }
+    
+    gvc.nextMove = function() {
+        
+    }
+    
+    gvc.previousMove = function() {
+        
+    }
+    
+}
+//==========================================================================================================================//
+// ProfileHelper                                                                                                       
+//==========================================================================================================================//
+function PlayerManager(player) {
+    
+}
+//==========================================================================================================================//
+// Launch                                                                                                        
+//==========================================================================================================================//
+$(document).ready(function() {
+    vc.launch();
+});
